b9a4b4318a3f86cc476ef04ff2062f9d1ba2f317
seym45 <seym@anyconnect.com>
2020-07-30 00:56:47 +0600

    Updates

---
+++ precache-manifest.54d5bdafa0402559ebebc6114cff0d4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edabbd633e6a641bc12c74303febe75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57015c3a9b52d01f5ae0",
+    "url": "/static/css/main.1305ebaa.chunk.css"
+  },
+  {
+    "revision": "ad1722273c3ee7c246e2",
+    "url": "/static/js/2.08f97900.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.08f97900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57015c3a9b52d01f5ae0",
+    "url": "/static/js/main.7ac39d4e.chunk.js"
+  },
+  {
+    "revision": "b922ef066b2364a2795a",
+    "url": "/static/js/runtime-main.e43a6e6c.js"
+  }
+]);
